0148a7ec573093c50788509717080a9dd926f896
Rob <hi@robinscharf.me>
2020-10-06 10:13:13 -0400

    procfile cleanup

--- app.py
+++ app.py
@@ -120,8 +120,6 @@ def static_file(path):
     static_folder = os.path.join(os.getcwd(), 'static')
     return send_from_directory(static_folder, path)
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
 
 
 
